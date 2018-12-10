297ee80e0f8548274e79a5696007f3d1178bffd6
chetan <chetanag35@gmail.com>
2018-12-10 12:15:54 +0530

    Space issues

--- node-express/route/dishRouter.js
+++ node-express/route/dishRouter.js
@@ -6,37 +6,37 @@ const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.all((req,res,next)=>{
+.all((req,res,next) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type','text/plain');
 	next();
 })
-.get((req,res,next)=>{
+.get((req,res,next) => {
 	res.end('Will send all dish to you!');
 })
-.post((req,res,next)=>{
+.post((req,res,next) => {
 	res.end('Will add the dish '+ req.body.name+ ' with description ' + req.body.description);
 })
-.put((req,res,next)=>{
+.put((req,res,next) => {
 	res.statusCode = 403;
 	res.end('PUT opereation not supported in /dishes');
 })
-.delete((req,res,next)=>{
+.delete((req,res,next) => {
 	res.end('Deleting all the dishes');
 }); 
 
 dishRouter.route('/:dishId')
-.get((req,res,next)=>{
+.get((req,res,next) => {
 	res.end('Will send ' + req.params.dishId + ' to you!');
 })
-.post((req,res,next)=>{
+.post((req,res,next) => {
 	res.end('POST operation is not supported in '  + req.params.dishId);
 })
-.put((req,res,next)=>{
+.put((req,res,next) => {
 	res.statusCode = 403;
 	res.end('Dish ' + req.params.dishId + ' will be updated');
 })
-.delete((req,res,next)=>{
+.delete((req,res,next) => {
 	res.end('Deleting ' + req.params.dishId + ' dish ');
 });
 
